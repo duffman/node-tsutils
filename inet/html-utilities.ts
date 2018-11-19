0d0c332a341e7a6bf5eda16e3787454edb0e6a22
duffman <patrik.forsberg@coldmind.com>
2018-11-19 01:51:26 +0100

    Added new stuff

--- inet/html-utilities.ts
+++ inet/html-utilities.ts
@@ -2,13 +2,13 @@
  * Copyright (C) Patrik Forsberg <patrik.forsberg@coldmind.com> - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
- * September 2018
  */
 
-import * as striptags from "striptags";
+//import * as striptags from "striptags";
 
 export class HtmlUtilities {
-	public static stripTags(input: string): string {
-		return striptags(input);
+	public static stripTags(htmlData: string): string {
+		return htmlData.replace(/(<([^>]+)>)/ig,"");
+		//return striptags(input);
 	}
 }
